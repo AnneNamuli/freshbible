de4832dea747e9871b4365ad48342a90d47f8f67
Anne Namuli <anne.namuli@gmail.com>
2021-11-11 04:37:47 +0200

    autopupolate books of the bible

---
+++ backend/app/app/autopopulate_books.py
@@ -0,0 +1,55 @@
+from docx import Document
+
+doc = Document("Fresh Word (Full Version).docx")
+paragraphs = (doc.paragraphs)
+
+books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', 'Ezekiel', '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah',
+         'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']
+
+
+# cleaning for ezekial -- has number that are not chapter - Ezekiel removed
+chapters = []
+
+
+def get_chapters_dict():
+    chapter_dict = {}
+    prev_chapter = None
+    prev_chapter_index = 1
+
+    for book_name in books:
+        for idx, paragraph in enumerate(paragraphs):
+            if paragraph.text.startswith(book_name):
+                chapters.append(paragraph.text)
+                new_chapter = paragraph.text
+                new_chapter_index = idx  # save pos
+
+                if prev_chapter == None:
+                    prev_chapter = new_chapter
+                    continue
+
+                if new_chapter != prev_chapter:
+                    chapter_dict[prev_chapter] = get_chapter_text(
+                        start_index=prev_chapter_index,
+                        stop_index=new_chapter_index)
+
+                    # move to the next chapter
+                    prev_chapter = new_chapter
+                    prev_chapter_index = new_chapter_index + 1
+
+    # For the very last one i.e Revelation 22
+    chapter_dict[prev_chapter] = get_chapter_text(
+        start_index=prev_chapter_index,
+        stop_index=len(paragraphs))
+
+    return chapter_dict
+
+
+def get_chapter_text(start_index, stop_index):
+    text = map(
+        lambda paragraph: paragraph.text,
+        paragraphs[start_index:stop_index])
+
+    return ' '.join(list(text))
+
+
+result = get_chapters_dict()
--- backend/app/app/backend_pre_start.py
+++ backend/app/app/backend_pre_start.py
@@ -2,6 +2,9 @@ import logging
 
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
+from sqlalchemy.orm import Session
+from app.models.db_models import Bible_Book, Bible_Chapter, Bible_Verse
+
 from app.db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
@@ -27,9 +30,90 @@ def init() -> None:
         raise e
 
 
+def create_books(db: SessionLocal()) -> Bible_Book:
+    try:
+        books = [
+            Bible_Book(title="Genesis", slug="GEN"),
+            Bible_Book(title="Exodus", slug="EXO"),
+            Bible_Book(title="Leviticus", slug="LEV"),
+            Bible_Book(title="Numbers", slug="NUM"),
+            Bible_Book(title="Deuteronomy", slug="DEU"),
+            Bible_Book(title="Joshua", slug="JOS"),
+            Bible_Book(title="Judges", slug="JDG"),
+            Bible_Book(title="Ruth", slug="RUT"),
+            Bible_Book(title="1Samuel", slug="1SA"),
+            Bible_Book(title="2Samuel", slug="2SA"),
+            Bible_Book(title="1Kings", slug="1KI"),
+            Bible_Book(title="2Kings", slug="2KI"),
+            Bible_Book(title="1Chronicles", slug="1CH"),
+            Bible_Book(title="2Chronicles", slug="2CH"),
+            Bible_Book(title="Ezra", slug="EZR"),
+            Bible_Book(title="Nehemiah", slug="NEH"),
+            Bible_Book(title="Esther", slug="EST"),
+            Bible_Book(title="Job", slug="JOB"),
+            Bible_Book(title="Psalms", slug="PSA"),
+            Bible_Book(title="Proverbs", slug="PRO"),
+            Bible_Book(title="Ecclesiastes", slug="ECC"),
+            Bible_Book(title="SongofSongs", slug="SNG"),
+            Bible_Book(title="Isaiah", slug="ISA"),
+            Bible_Book(title="Jeremiah", slug="JER"),
+            Bible_Book(title="Lamentations", slug="LAM"),
+            Bible_Book(title="Ezekiel", slug="EZK"),
+            Bible_Book(title="Daniel", slug="DAN"),
+            Bible_Book(title="Hosea", slug="HOS"),
+            Bible_Book(title="Joel", slug="JOL"),
+            Bible_Book(title="Amos", slug="AMO"),
+            Bible_Book(title="Obadiah", slug="OBA"),
+            Bible_Book(title="Jonah", slug="JON"),
+            Bible_Book(title="Micah", slug="MIC"),
+            Bible_Book(title="Nahum", slug="NAM"),
+            Bible_Book(title="Habakkuk", slug="HAB"),
+            Bible_Book(title="Zephaniah", slug="ZEP"),
+            Bible_Book(title="Haggai", slug="HAG"),
+            Bible_Book(title="Zechariah", slug="ZEC"),
+            Bible_Book(title="Malachi", slug="MAL"),
+            Bible_Book(title="Matthew", slug="MAT"),
+            Bible_Book(title="Mark", slug="MRK"),
+            Bible_Book(title="Luke", slug="LUK"),
+            Bible_Book(title="John", slug="JHN"),
+            Bible_Book(title="Acts", slug="ACT"),
+            Bible_Book(title="Romans", slug="ROM"),
+            Bible_Book(title="1Corinthians", slug="1CO"),
+            Bible_Book(title="2Corinthians", slug="2CO"),
+            Bible_Book(title="Galatians", slug="GAL"),
+            Bible_Book(title="Ephesians", slug="EPH"),
+            Bible_Book(title="Philippians", slug="PHP"),
+            Bible_Book(title="Colossians", slug="COL"),
+            Bible_Book(title="1Thessalonians", slug="1TH"),
+            Bible_Book(title="2Thessalonians", slug="2TH"),
+            Bible_Book(title="1Timothy", slug="1TI"),
+            Bible_Book(title="2Timothy", slug="2TI"),
+            Bible_Book(title="Titus", slug="TIT"),
+            Bible_Book(title="Philemon", slug="PHM"),
+            Bible_Book(title="Hebrews", slug="HEB"),
+            Bible_Book(title="James", slug="JAS"),
+            Bible_Book(title="1Peter", slug="1PE"),
+            Bible_Book(title="2Peter", slug="2PE"),
+            Bible_Book(title="1John", slug="1JN"),
+            Bible_Book(title="2John", slug="2JN"),
+            Bible_Book(title="3John", slug="3JN"),
+            Bible_Book(title="Jude", slug="JUD"),
+            Bible_Book(title="Revelation", slug="REV")
+        ]
+
+        db.bulk_save_objects(books)
+        db.commit()
+
+        logging.info("Creating books of the bible")
+
+    except:
+        logging.error('NOT WORKING')
+
+
 def main() -> None:
     logger.info("Initializing service")
     init()
+    create_books(db=SessionLocal())
     logger.info("Service finished initializing")
 
 
