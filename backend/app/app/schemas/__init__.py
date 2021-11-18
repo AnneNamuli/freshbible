be79ffc24aab9530cfe72dba5c77b2e08e1617fa
Anne Namuli <anne.namuli@gmail.com>
2021-11-18 10:41:55 +0000

    Db schema models

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -4,3 +4,5 @@ from .user import User, UserCreate, UserInDB, UserUpdate
 from .bible_books import BibleBookCreate, BibleBookInDB, BibleBookUpdate, BibleBook
 from .bible_chapter import BibleChapterCreate, BibleChapterInDB, BibleChapterUpdate, BibleChapter
 from .bible_verse import BibleVerse, BibleVerseCreate, BibleVerseInDB, BibleVerseUpdate
+from .organization import Organization, OrganizationCreate, OrganizationUpdate, OrganizationInDB
+from .organization_user import OrganizationUser, OrganizationUserCreate, OrganizationUserUpdate, OrganizationUserInDB
