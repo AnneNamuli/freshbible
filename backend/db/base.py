1040a9e23f9d01f576ec7a9f5614bba96de9da5d
Anne Namuli <anne.namuli@gmail.com>
2021-11-26 01:54:17 +0200

    updates

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,4 +1,4 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
-from db.base_class import Base  # noqa
-from models.db_models import *  # noqa
+from backend.db.base_class import Base  # noqa
+from backend.models.db_models import *  # noqa
