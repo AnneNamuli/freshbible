fc76c47ad7860b32113dcae7b011630ce59b8379
Anne Namuli <anne.namuli@gmail.com>
2021-11-26 02:28:21 +0200

    updates

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.endpoints import login, users, utils, bible,\
+from backend.api.endpoints import login, users, utils, bible,\
     bible_chapter, bible_verse, organization, organization_user
 
 api_router = APIRouter()
