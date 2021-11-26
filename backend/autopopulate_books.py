49405d838832a8f2e5d264d8460055d7789c0464
Anne Namuli <anne.namuli@gmail.com>
2021-11-26 10:50:01 +0200

    fix

--- backend/autopopulate_books.py
+++ backend/autopopulate_books.py
@@ -6,16 +6,9 @@ import os
 
 from docx import Document
 
-try:
-    doc = Document("FresWord.docx")
-    paragraphs = (doc.paragraphs)
-except:
-    doc = Document()
-    doc.save('FresWordEdit.docx')
-    print("Previous file was corrupted or didn't exist - new file was created.")
-
-doc = Document("FresWord.docx")
+doc = Document("FreshWord.docx")
 paragraphs = (doc.paragraphs)
+
 
 books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', 'Ezekiel', '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah',
          'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']
