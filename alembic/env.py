784c3d506ea7967f9e9727d333c731cf01eb7ed6
Anne Namuli <anne.namuli@gmail.com>
2021-11-26 02:14:57 +0200

    updates

--- alembic/env.py
+++ alembic/env.py
@@ -35,7 +35,7 @@ def get_url():
     # password = os.getenv("POSTGRES_PASSWORD", "")
     # server = os.getenv("POSTGRES_SERVER", "db")
     # db = os.getenv("POSTGRES_DB", "app")
-    return f"postgres://ajwxdwkkfxevyz:bb8cce02280090a8f1ad70d0f84447be1624eed4d109a5b3d78461deb3b12f62@ec2-50-16-241-192.compute-1.amazonaws.com:5432/dd3l6uitsvdd3t"
+    return f"postgresql://ajwxdwkkfxevyz:bb8cce02280090a8f1ad70d0f84447be1624eed4d109a5b3d78461deb3b12f62@ec2-50-16-241-192.compute-1.amazonaws.com:5432/dd3l6uitsvdd3t"
 
 
 def run_migrations_offline():
