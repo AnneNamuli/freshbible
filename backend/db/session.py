01d2484d95742c31668de2d13d8542351781d165
Anne Namuli <anne.namuli@gmail.com>
2021-11-26 02:06:07 +0200

    updates

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,5 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_engine(
+    'postgres://ajwxdwkkfxevyz:bb8cce02280090a8f1ad70d0f84447be1624eed4d109a5b3d78461deb3b12f62@ec2-50-16-241-192.compute-1.amazonaws.com:5432/dd3l6uitsvdd3t', pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
